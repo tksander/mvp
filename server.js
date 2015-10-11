b1233acbadd7729a5ee3036abc35321ba8da2063
tksander <tkepasander@gmail.com>
2015-10-11 09:41:22 -0700

    Moved models/views around for server structure

--- server.js
+++ server.js
@@ -4,7 +4,19 @@ var path = require('path');
 var request = require('request');
 
 var app = express();
+console.log(__dirname + '/index.html');
+// app.use(express.static(__dirname + '/assets/*'));
+// app.use(express.static(__dirname + '/css/*'));
+// app.use(express.static(__dirname + '/js/**/*'));
+app.use(express.static(__dirname + '/public'));
+console.log(__dirname + '/public');
 
+
+
+
+////////////////////////////////////
+//      Authorization 
+////////////////////////////////////
 
 // Strava credentials
 var clientId = '8126'
@@ -19,12 +31,15 @@ redirectPath += '&response_type=code'
 redirectPath += '&redirect_uri=' + redirectUrl;
 redirectPath += '&approval_prompt=force';
 
-console.log(redirectPath);
 
-// Home page
-app.get('/', function(req, res) {
-  res.send("home page")
-});
+////////////////////////////////////
+//      Paths 
+////////////////////////////////////
+
+// // Home page
+// app.get('/', function(req, res) {
+//   res.render('index')
+// });
 
 
 // Route for strava auth
@@ -38,14 +53,16 @@ app.get('/login', function(req, res) {
   res.send('you\'ve arrived');
 });
 
-
-
 // Get request 
 request('http://www.google.com', function (error, response, body) {
   if (!error && response.statusCode == 200) {
     console.log("found") // Show the HTML for the Google homepage. 
   }
 })
+
+////////////////////////////////////
+//      Server Start 
+/////////////////////////////////////
 
 console.log('MVP Server is listening on 4568');
 app.listen(4568);
