ebf713c31f603dd4acb76daa9866eee50d55bd0b
tksander <tkepasander@gmail.com>
2016-01-17 21:59:35 -0800

    Should remove localtokens

--- localTokens.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  access_token  : "491eafe37f405331ffc91eef2d04209cbd74f9f5",
-  client_id     : "8126",
-  client_secret : "b7889c23767b70ea95b432c78f20ea79c2ac5af1",
-  redirect_uri  : "http://127.0.0.1:4568/login"
-}
